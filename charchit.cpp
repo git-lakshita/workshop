c52cb923e3b9529de9867e4416affa7ec9375547
charchit95 <charchit95@gmail.com>
2019-09-07 12:25:43 +0530

    1st version

---
+++ charchit.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+int main()
+{
+    int a=10;
+    cout<<"Using Git & Github";
+    cout<<"Basics of Git and Github";
+    cout<<a;
+    //This is first version......
+    return 0;
+}
